2eb303f99801b0c04dbdc31e25dd5cccd1e48c93
Samir Poudel <samirpoudel03@gmail.com>
2024-04-16 13:33:59 +0545

    Step-13

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:myapp1/constants/routes.dart';
 import 'package:myapp1/services/auth/auth_service.dart';
 import 'package:myapp1/views/login_view.dart';
-import 'package:myapp1/views/notes_view.dart';
+import 'package:myapp1/views/notes/new_note_view.dart';
+import 'package:myapp1/views/notes/notes_view.dart';
 import 'package:myapp1/views/register_view.dart';
 import 'package:myapp1/views/verify_email_view.dart';
 
@@ -21,6 +22,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
+        newNoteRoute: (context) => const NewNoteView(),
       },
     ),
   );
--- test/auth_test.dart
+++ test/auth_test.dart
@@ -113,7 +113,10 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'foo@bar.com') throw CredentialNotFoundAuthException();
     if (password == 'foobar') throw CredentialNotFoundAuthException();
-    const user = AuthUser(isEmailVerified: false);
+    const user = AuthUser(
+      email: 'foo@bar.com',
+      isEmailVerified: false,
+    );
     _user = user;
     return Future.value(user);
   }
@@ -131,7 +134,10 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw CredentialNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true);
+    const newUser = AuthUser(
+      isEmailVerified: true,
+      email: 'foo@bar.com',
+    );
     _user = newUser;
   }
 }
